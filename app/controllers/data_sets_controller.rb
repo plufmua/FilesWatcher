8e886c52d05aac7a5c45c775d411fb510a864382
nadezhda.zharko <nadezhda.zharko@gmail.com>
2017-02-27 14:53:01 +0300

    Fix method to delete files from database

--- app/controllers/data_sets_controller.rb
+++ app/controllers/data_sets_controller.rb
@@ -10,7 +10,8 @@ class DataSetsController < ApplicationController
   end
   # method to update files in database
   def update_files
-    paths = [ "/home/zharko/Untitled Folder", "/home/zharko/Untitled Folder" ]
+    paths = [ "/home/zharko/Untitled Folder", "/home/zharko/qewe" ]
+    @data_sets = DataSet.all.to_a
     paths.each do |path|
       if Dir.exist? path
         Dir.glob(File.join(path, '**', '*')).each { |file|
@@ -18,17 +19,25 @@ class DataSetsController < ApplicationController
             data_set = DataSet.new
             data_set.name = file.split('/')[-1]
             data_set.size = File.size(file)
-            data_set.absolute_path = file
-            data_set.updating_time = File.mtime file
+            data_set.absolute_path = file[0..file.rindex('/')]
+            data_set.updating_time = File.mtime(file).to_datetime
             data_set.owner = File.stat(file).uid
             data_set.group = File.stat(file).gid
             data_set.permissions = File.stat(file).mode.to_s(8).to_i%1000
-            data_set.save!
+            if DataSet.exists?(name: data_set.name, absolute_path: data_set.absolute_path)
+              # data_set.update_attributes!
+              @data_sets.delete_if{ |dataset| dataset.name == data_set.name && dataset.absolute_path == data_set.absolute_path}
+            else
+              data_set.save
+            end
           end
         }
       else
         next
       end
+    end
+    @data_sets.each do |dataset|
+      dataset.delete
     end
     redirect_to root_path
   end
--- app/models/data_set.rb
+++ app/models/data_set.rb
@@ -6,9 +6,9 @@ class DataSet < ApplicationRecord
   validates :owner, presence: true, length: { maximum: 255 }
   validates :group, presence: true, length: { maximum: 255 }
   validates :permissions, presence: true, numericality: { only_integer: true, greater_than: 0, less_than_or_equal_to: 777 }
-  validate 'valid_updating_time'
-
-  def valid_updating_time
-    errors.add(:updating_time, "Invalid updating date") unless updating_time.is_a?(DateTime)
-  end
+  # validate 'valid_updating_time'
+  #
+  # def valid_updating_time
+  #   errors.add(:updating_time, "Invalid updating date") unless :updating_time.is_a?(DateTime)
+  # end
 end
