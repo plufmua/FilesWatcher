7b1878fb54bfe53f3196c9f4355179119bf00d0c
nadezhda.zharko <nadezhda.zharko@gmail.com>
2017-02-27 16:10:20 +0300

    Fix method to update info in database

--- app/controllers/data_sets_controller.rb
+++ app/controllers/data_sets_controller.rb
@@ -1,5 +1,5 @@
 class DataSetsController < ApplicationController
-  require 'find'
+  require 'etc'
 
   def index
     @data_sets = DataSet.page(params[:page])
@@ -21,14 +21,15 @@ class DataSetsController < ApplicationController
             data_set.size = File.size(file)
             data_set.absolute_path = file[0..file.rindex('/')]
             data_set.updating_time = File.mtime(file).to_datetime
-            data_set.owner = File.stat(file).uid
-            data_set.group = File.stat(file).gid
+            data_set.owner = Etc.getpwuid(File.stat(file).uid).name
+            data_set.group = Etc.getgrgid(File.stat(file).gid).name
             data_set.permissions = File.stat(file).mode.to_s(8).to_i%1000
             if DataSet.exists?(name: data_set.name, absolute_path: data_set.absolute_path)
-              # data_set.update_attributes!
+              DataSet.find_by(name: data_set.name, absolute_path: data_set.absolute_path).
+                  update_attributes({size: data_set.size, updating_time: data_set.updating_time, owner: data_set.owner, group: data_set.group, permissions: data_set.permissions})
               @data_sets.delete_if{ |dataset| dataset.name == data_set.name && dataset.absolute_path == data_set.absolute_path}
             else
-              data_set.save
+              data_set.save!
             end
           end
         }
