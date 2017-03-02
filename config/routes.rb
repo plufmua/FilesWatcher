0c6b31748c4d8c4a208333aff201ac5451a3f46f
nadezhda.zharko <nadezhda.zharko@gmail.com>
2017-03-02 11:03:50 +0300

    Change route for button to update files

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :data_sets, only: [:index, :show] do
-    get 'update_files', on: :collection
+    post 'update_files', on: :collection
   end
   root to: 'data_sets#index'
 end
