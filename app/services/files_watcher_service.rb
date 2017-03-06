29471d392a9b8d3a4cfbb45d031f5dcddf09cb9d
nadezhda.zharko <nadezhda.zharko@gmail.com>
2017-03-06 16:36:26 +0300

    Compare collections by ids

--- app/services/files_watcher_service.rb
+++ app/services/files_watcher_service.rb
@@ -1,6 +1,6 @@
 class FilesWatcherService
 
-  CREATABLE_ATTRIBUTES = %i(name absolute_path)
+  CREATABLE_ATTRIBUTES = %i(name absolute_path).freeze
   UPDATABLE_ATTRIBUTES = %i(size updating_time owner group permissions).freeze
 
   def initialize
@@ -17,7 +17,10 @@ class FilesWatcherService
       end
     end
 
-    @data_sets_from_database = DataSet.all
+    @data_sets_from_database = DataSet.all.to_a
+    processed_ids = @processed_data_sets.map {|data_set| data_set.id }
+    @data_sets_from_database.reject {|data_set| processed_ids.include? data_set.id}
+
     DataSet.where(id: @data_sets_from_database - @processed_data_sets).delete_all
   end
 
@@ -25,26 +28,27 @@ class FilesWatcherService
 
   def update_tracked_file(attributes)
     data_set = DataSet.find_by(attributes.slice(*CREATABLE_ATTRIBUTES))
-    if data_set.nil?
-      data_set = DataSet.new(attributes)
-      data_set.save!
+
+    if data_set
+      data_set.update_attributes!(attributes.slice(*UPDATABLE_ATTRIBUTES))
     else
-      data_set.update_attributes(attributes.slice(*UPDATABLE_ATTRIBUTES))
+      data_set = DataSet.create!(attributes)
     end
+
     @processed_data_sets.push data_set
   end
 
   def initialize_data_set(file)
     data_set = {}
 
-    data_set.tap do |attributes|
-      attributes[:name] = File.basename file
-      attributes[:size] = File.size file
-      attributes[:absolute_path] = File.dirname file
-      attributes[:updating_time] = File.mtime(file).to_datetime
-      attributes[:owner] = Etc.getpwuid(File.stat(file).uid).name
-      attributes[:group] = Etc.getgrgid(File.stat(file).gid).name
-      attributes[:permissions] = File.stat(file).mode.to_s(8).to_i % 1000
+    data_set.tap do |attribute|
+      attribute[:name] = File.basename file
+      attribute[:size] = File.size file
+      attribute[:absolute_path] = File.dirname file
+      attribute[:updating_time] = File.mtime(file).to_datetime
+      attribute[:owner] = Etc.getpwuid(File.stat(file).uid).name
+      attribute[:group] = Etc.getgrgid(File.stat(file).gid).name
+      attribute[:permissions] = File.stat(file).mode.to_s(8).to_i % 1000
     end
 
   end
