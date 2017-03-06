e80ae2bd82e5ba26600f32ac4be7acbd6b16a6a0
nadezhda.zharko <nadezhda.zharko@gmail.com>
2017-03-06 11:20:24 +0300

    Fix updating records

--- app/services/files_watcher_service.rb
+++ app/services/files_watcher_service.rb
@@ -14,7 +14,7 @@ class FilesWatcherService
     end
 
     @data_sets_from_database = DataSet.all
-    pp @delete_from_database = @data_sets_from_database - (@data_sets & @data_sets_from_database)
+    @delete_from_database = @data_sets_from_database - (@data_sets & @data_sets_from_database)
 
     DataSet.where(id: @delete_from_database).delete_all
   end
@@ -22,8 +22,14 @@ class FilesWatcherService
   private
 
   def update_tracked_file(data_set)
-    # DataSet.find_or_create_by(data_set)
-    @data_sets.push DataSet.find_or_create_by(data_set)
+    dataset = DataSet.find_or_create_by(data_set)
+    dataset.update_attributes(size: data_set[:size],
+                   updating_time: data_set[:updating_time],
+                   owner: data_set[:owner],
+                   group: data_set[:group],
+                   permissions: data_set[:permissions]
+    ) if dataset.persisted?
+    @data_sets.push dataset
   end
 
   def initialize_data_set(file)
